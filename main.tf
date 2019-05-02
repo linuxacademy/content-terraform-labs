68c2962e18022c5196a3054f904c5c771e37ed1c
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2019-05-02 09:56:34 -0500

    update port

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "docker_container" "container_id" {
   name  = "${var.container_name}"
   image = "${docker_image.image_id.latest}"
   ports {
-    internal = "${var.int_port}"
+    internal = "2368"
     external = "${var.ext_port}"
   }
 }
